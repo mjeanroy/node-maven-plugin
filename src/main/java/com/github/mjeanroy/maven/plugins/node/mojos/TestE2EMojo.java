4cc56899673d6fb062c4953fb4bbb15e6773f6da
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2015-10-26 21:03:42 +0100

    feat: add test-e2e mojo

---
+++ src/main/java/com/github/mjeanroy/maven/plugins/node/mojos/TestE2EMojo.java
@@ -0,0 +1,82 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.maven.plugins.node.mojos;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import static com.github.mjeanroy.maven.plugins.node.commons.ObjectUtils.firstNonNull;
+
+/**
+ * Test End 2 End Mojo.
+ * Basically, it only runs `npm run-script test-e2e`.
+ * Execution will be logged to the console.
+ *
+ * This mojo will run automatically during the test phase and does not
+ * require online connection.
+ */
+@Mojo(
+	name = "test-e2e",
+	defaultPhase = LifecyclePhase.INTEGRATION_TEST,
+	requiresOnline = false
+)
+public class TestE2EMojo extends AbstractNpmScriptMojo {
+
+	/**
+	 * Set test-e2e mojo to custom npm script.
+	 */
+	@Parameter(defaultValue = "${npm.script.testE2E}", required = false)
+	private String script;
+
+	/**
+	 * Check if end to end tests must be skipped.
+	 * By default, end to end tests are skipped if maven.test.skip property is set to true.
+	 */
+	@Parameter(defaultValue = "${maven.test.skip}", required = false)
+	private boolean skipTests;
+
+	/**
+	 * Flag to skip mojo execution.
+	 */
+	@Parameter(defaultValue = "${npm.skip.testE2E}", required = false)
+	private boolean skip;
+
+	/**
+	 * Create Mojo.
+	 */
+	public TestE2EMojo() {
+		super();
+	}
+
+	@Override
+	protected String getScript() {
+		return firstNonNull(script, "test-e2e");
+	}
+
+	@Override
+	protected boolean isSkipped() {
+		return skipTests || skip;
+	}
+}
---
+++ src/test/java/com/github/mjeanroy/maven/plugins/node/mojos/TestE2EMojoTest.java
@@ -0,0 +1,60 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.maven.plugins.node.mojos;
+
+import com.github.mjeanroy.maven.plugins.node.commands.Command;
+import com.github.mjeanroy.maven.plugins.node.commands.CommandExecutor;
+import org.apache.maven.plugin.logging.Log;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.apache.commons.lang3.reflect.FieldUtils.readField;
+import static org.apache.commons.lang3.reflect.FieldUtils.writeField;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class TestE2EMojoTest extends AbstractNpmScriptMojoTest<TestE2EMojo> {
+
+	@Override
+	protected String mojoName() {
+		return "test-e2e";
+	}
+
+	@Test
+	public void it_should_skip_tests() throws Exception {
+		TestE2EMojo mojo = createMojo("mojo-with-parameters", true);
+		writeField(mojo, "skipTests", true, true);
+
+		CommandExecutor executor = (CommandExecutor) readField(mojo, "executor", true);
+		Log logger = (Log) readField(mojo, "log", true);
+
+		mojo.execute();
+
+		verify(executor, never()).execute(any(File.class), any(Command.class), eq(logger));
+		verify(logger).info("Npm test-e2e is skipped.");
+	}
+}
